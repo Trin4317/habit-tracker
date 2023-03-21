6ab1d00f09e636d827258d3f22587f087c7784ef
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-21 20:09:26 +0900

    create Habit model with utility classes

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Habit;
+use Illuminate\Http\Request;
+
+class HabitController extends Controller
+{
+    public function index()
+    {
+        $habits = Habit::all();
+
+        return view('habits.index', compact('habits'));
+    }
+}
