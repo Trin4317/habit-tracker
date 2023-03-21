e9f5595623116403ad0f3daa7a830f422f88f986
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-21 20:10:47 +0900

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HabitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/habits', [HabitController::class, 'index'])->name('habits.index');
