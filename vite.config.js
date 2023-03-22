cfc9a2a798439a4a33df852b2a9abfccf349bd6d
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-22 15:56:21 +0000

    config vite and npm

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     // Allow connection to Vite server inside Docker container from host machine
@@ -14,5 +16,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
+    test: {
+        environment: 'jsdom',
+    },
 });
