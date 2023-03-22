0983932c7e391668031eed1f7e5abc4c5689549e
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-22 15:57:06 +0000

    create first Vue component

---
+++ resources/js/components/HabitInfo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col basis-1/2">
+        <h1 id="name" class="text-base font-semibold">{{ name }}</h1>
+        <small class="font-normal text-gray-400">1 / 3 times</small>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        name: {
+            type: String
+        }
+    })
+</script>
