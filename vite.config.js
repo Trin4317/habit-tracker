15aa9a1594fc750d1a3ae99b3d34350790eb42ee
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-22 14:39:04 +0000

    config vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    // Allow connection to Vite server inside Docker container from host machine
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
