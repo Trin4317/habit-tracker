7fa7fd1504646527460e40179515d5f8a432bfea
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-25 00:50:34 +0900

    fix variable name

--- resources/js/components/HabitInfo.vue
+++ resources/js/components/HabitInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col basis-1/2">
         <h1 id="name" class="text-base font-semibold">{{ name }}</h1>
-        <small id="execution" class="font-normal text-gray-400">{{ execution_count }} / {{ times_per_day }} times</small>
+        <small id="execution" class="font-normal text-gray-400">{{ executions_count }} / {{ times_per_day }} times</small>
     </div>
 </template>
 
@@ -13,7 +13,7 @@
         times_per_day: {
             type: Number
         },
-        execution_count: {
+        executions_count: {
             type: Number
         }
     })
