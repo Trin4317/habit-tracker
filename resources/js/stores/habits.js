ab78cfc916596c59c5fdab8cffa0eab02bc078dd
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-25 14:32:54 +0000

    add store habit action and update Habit Dialog component

--- resources/js/stores/habits.js
+++ resources/js/stores/habits.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export const useHabitsStore = defineStore('habits', () => {
     const list = ref([])
     const isDialogOpen = ref(false)
+    const validationErrors = ref({})
+    const formData = reactive({
+        id: '',
+        name: '',
+        times_per_day: ''
+    })
 
     const fetch = async () => {
         try {
@@ -35,13 +41,33 @@ export const useHabitsStore = defineStore('habits', () => {
         isDialogOpen.value = false
     }
 
+    const storeHabit = async () => {
+        try {
+            let response = await axios.post('/api/habits', formData)
+
+            validationErrors.value = {}
+            formData.name = ''
+            formData.times_per_day = ''
+
+            list.value = response.data.data
+            closeDialog()
+        } catch (error) {
+            if (error.response.status == 422) {
+                validationErrors.value = error.response.data.errors
+            }
+        }
+    }
+
     return {
         list,
         isDialogOpen,
+        validationErrors,
+        formData,
         fetch,
         newExecution,
         percent,
         openDialog,
         closeDialog,
+        storeHabit,
     }
 })
